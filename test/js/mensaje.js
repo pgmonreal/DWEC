a46f5e661153f7052b275f6f4b8cc9ad7d560acf
Pedro Garcia <pgmonre@gmail.com>
2017-09-19 20:46:00 +0200

    Primeras pruebas con JS

---
+++ test/js/mensaje.js
@@ -0,0 +1,6 @@
+alert(t);
+console.log("Esto es una cadena.");
+console.info("Esto es info.");
+console.warn("Esto es un aviso.");
+console.error("Esto es un error.");
+document.write("<h1>Formateo.</h1>")
