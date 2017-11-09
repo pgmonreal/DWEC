1b6b87298427eb57fadfb659779d8ef0751fd89c
Pedro <pgmonre@gmail.com>
2017-11-09 22:03:20 +0100

    Subida Practica Ejercicios 1 - Unidad 3

--- U3/P1/E1/JS/u3p1e1.js
+++ U3/P1/E1/JS/u3p1e1.js
@@ -1,8 +1,8 @@
 var body = document.body.children;
 
 body[0].innerHTML = "Esto es un fiesta.";
-body[3].innerHTML = "Esto era un ejemplo.";
-body[4].innerHTML = "Esto ya no se ni lo que era...";
+body[1].innerHTML = "Esto era un ejemplo.";
+body[2].innerHTML = "Esto ya no se ni lo que era...";
 
 document.title = "Nuevo titulo.";
 
